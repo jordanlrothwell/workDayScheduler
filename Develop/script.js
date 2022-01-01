3416e11660e1ee419216535a5cf65125cbb0d9d9
Jordan Rothwell <jordanlrothwell@gmail.com>
2022-01-01 12:26:50 +1000

    Added function to create savebutton.

--- Develop/script.js
+++ Develop/script.js
@@ -1,43 +1,43 @@
 let today = moment();
 
 let dayPlan = [
-    {
-        thisHour: "09",
-        activity: "",
-    },
-    {
-        thisHour: "10",
-        activity: "",
-    },
-    {
-        thisHour: "11",
-        activity: "",
-    },
-    {
-        thisHour: "12",
-        activity: "",
-    },
-    {
-        thisHour: "13",
-        activity: "",
-    },
-    {
-        thisHour: "14",
-        activity: "",
-    },
-    {
-        thisHour: "15",
-        activity: "",
-    },
-    {
-        thisHour: "16",
-        activity: "",
-    },
-    {
-        thisHour: "17",
-        activity: "",
-    }
-]
+  {
+    thisHour: "09",
+    activity: "",
+  },
+  {
+    thisHour: "10",
+    activity: "",
+  },
+  {
+    thisHour: "11",
+    activity: "",
+  },
+  {
+    thisHour: "12",
+    activity: "",
+  },
+  {
+    thisHour: "13",
+    activity: "",
+  },
+  {
+    thisHour: "14",
+    activity: "",
+  },
+  {
+    thisHour: "15",
+    activity: "",
+  },
+  {
+    thisHour: "16",
+    activity: "",
+  },
+  {
+    thisHour: "17",
+    activity: "",
+  },
+];
 // function to update current day
 let updateDay = () => $("#currentDay").text(today.format("dddd, D MMMM YYYY"));
 
@@ -49,34 +49,44 @@ let momentify = (str) => moment(str + ":00:00", "HH").format("LT");
 
 // create row div + append to container
 let createRowDiv = () => {
-    let rowDiv = $("<div>").attr({"class": "row"});
-    return rowDiv
-}
+  let rowDiv = $("<div>").attr({ class: "row" });
+  return rowDiv;
+};
 
 // create hour div
 let createHourDiv = (i) => {
-    let hourDiv = $("<div>").attr({"class": "hour"});
-    hourDiv.text(momentify(dayPlan[i].thisHour));
-    return hourDiv
-}
+  let hourDiv = $("<div>").attr({ class: "hour" });
+  hourDiv.text(momentify(dayPlan[i].thisHour));
+  return hourDiv;
+};
 
 // create textarea
 let createTextArea = () => {
-    let textArea = $("<textarea>");
-    return textArea
-}
+  let textArea = $("<textarea>");
+  return textArea;
+};
+
+// create save button
+let createSaveButton = () => {
+  let saveButton = $("<button>");
+  return saveButton;
+};
 
 // wrapper function
 let newRow = (i) => {
-    return createRowDiv().append(createHourDiv(i), createTextArea())
-}
+  return createRowDiv().append(
+    createHourDiv(i),
+    createTextArea(),
+    createSaveButton()
+  );
+};
 
 // appender function
 let generateContent = (i) => {
-    $(".container").append(newRow(i))
-}
+  $(".container").append(newRow(i));
+};
 
 // generate content
 for (i = 0; i < dayPlan.length; i++) {
-    generateContent(i);
+  generateContent(i);
 }
